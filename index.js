8c2f380d8ad1540bce937652dfbbd17e33990fa0
Annie <annie-d@hotmail.ca>
2016-05-02 19:04:32 +0000

    exercise 3

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 var express = require('express');
 var app = express();
 
+// Exercise 1
 app.get('/hello', function (req, res) {
   res.send('<h1>Hello World!</h1>');
 });
 
+// Exercise 2
 app.get('/hello/:name', function (request, response) {
   response.send('<h1>Hello '+ request.params.name + " !</h1>");
 });
 
+// Exercise 3
+
+function add(num1, num2) {
+  return parseInt(num1, 10) + parseInt(num2, 10);
+}
+
+function mult(num1, num2) {
+  return parseInt(num1, 10) * parseInt(num2, 10);
+}
+
+function div(num1, num2) {
+  return parseInt(num1, 10) / parseInt(num2, 10);
+}
+
+function sub(num1, num2) {
+  return parseInt(num1, 10) - parseInt(num2, 10);
+}
+
+app.get('/calculator/:operation', function (request, response) {
+  var num1 = request.query.num1;
+  var num2 = request.query.num2;
+  if (request.params.operation === "add") {
+    response.send({
+      operation: request.params.operation,
+      firstNumber: num1,
+      secondNumber: num2,
+      total: JSON.stringify(add(num1, num2))
+    });
+  } else if (request.params.operation === "sub") {
+    response.send({
+      operation: request.params.operation,
+      firstNumber: num1,
+      secondNumber: num2,
+      total: JSON.stringify(sub(num1, num2))
+    });
+  } else if (request.params.operation === "mult") {
+    response.send({
+      operation: request.params.operation,
+      firstNumber: num1,
+      secondNumber: num2,
+      total: JSON.stringify(mult(num1, num2))
+    });
+  } else if (request.params.operation === "div") {
+    response.send({
+      operation: request.params.operation,
+      firstNumber: num1,
+      secondNumber: num2,
+      total: JSON.stringify(div(num1, num2))
+    });
+  } else {
+    response.status(400).send("<h1>Error 400: Bad request. Please enter a valid operation such as: add, sub, mult or div.</h1>");
+  }
+});
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
